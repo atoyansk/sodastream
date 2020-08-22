3f0428726b2328cdf444d807099ed111528dca7e
Airton Toyansk <atoyansk@gmail.com>
2020-08-23 01:41:05 +0300

    Routing sets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
+import { QaReportsComponent } from './qa-reports/qa-reports.component';
+import { FailureReportsComponent } from './failure-reports/failure-reports.component';
+import { ReportsComponent } from './reports/reports.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent },
+  { path: 'qa-reports', component: QaReportsComponent },
+  { path: 'failure-reports', component: FailureReportsComponent },
+  { path: 'reports', component: ReportsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
